1273cca5f9b3aee01dc483abcc2d99441b13e403
Ilya Gilmanov <igilmanov2005@gmail.com>
2025-05-09 13:26:00 +0400

    repairing_2

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -23,13 +23,11 @@ class IsSuperUserOrAdminOrReadOnly(IsSuperUserOrAdminOnly):
 class IsModerOrAdminOrAuthorOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return request.user.is_authenticated
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        if (
-            request.user.is_admin
-            or request.user.is_moderator
-            or request.method in permissions.SAFE_METHODS
-        ):
+        if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.author == request.user
+        return (obj.author == request.user
+                or request.user.is_moderator or request.user.is_admin)
